0ae65ab31869f2fad813b4a066939579babdfdb0
Sahil Atahar <sahilatahar@gmail.com>
2023-02-02 22:55:17 +0530

    Function Programs Added

---
+++ Functions/explaination.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+
+/*
+Syntax of function:
+
+return_type func_name() {
+    statement...
+    statement...
+    statement...
+
+    return data;
+}
+
+*/
+
+void print()
+{
+    printf("I am function");
+}
+
+int main()
+{
+    print();
+
+    // getch();
+    return 0;
+}
---
+++ Functions/factorial_of_n.c
@@ -0,0 +1,31 @@
+//Factorial of a function
+
+#include <stdio.h>
+#include <conio.h>
+
+int factorial(int); // function prototype
+
+int main()
+{
+    int n, fact;
+
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    fact = factorial(n); // fucntion calling
+
+    printf("Factorial of %d is %d\n", n, fact);
+
+    getch();
+    return 0;
+}
+
+int factorial(int num) // function defination
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i; // fact = fact * i;
+    }
+    return fact;
+}
